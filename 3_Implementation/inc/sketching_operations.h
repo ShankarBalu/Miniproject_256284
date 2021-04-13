22d6d380a0522b7b2bd5e11d6178314b9a669c39
ShankarBalu <shankarbalu.1999@gmail.com>
2021-04-13 12:06:40 +0530

    Updating requirements

---
+++ 3_Implementation/inc/sketching_operations.h
@@ -0,0 +1,45 @@
+/** 
+* @file sketching_operations.h
+* Printing the sketch of hangman as per the chances
+*
+*/
+#ifndef __SKETCHING_OPERATIONS_H__
+#define __SKETCHING_OPERATIONS_H__
+
+/**
+ * @brief This is a function which prints the sketch for the first wrong guess when called
+ * 
+ * @return An integer value to use for testing the function
+ */
+int sketcha();
+
+/**
+ * @brief This is a function which prints the sketch for the second wrong guess when called
+ * 
+ * @return An integer value to use for testing the function
+ */
+int sketchb();
+
+/**
+ * @brief This is a function which prints the sketch for the third wrong guess when called
+ * 
+ * @return An integer value to be used for testing the function
+ */
+int sketchc();
+
+
+/**
+ * @brief This is a function which prints the sketch for the fourth wrong guess when called
+ * 
+ * @return An integer value to be used for testing the function
+ */
+int sketchd();
+
+/**
+ * @brief This is a function which prints the sketch for the fifth wrong guess when called
+ * 
+ * @return An integer value to be used for testing the function
+ */
+int sketche();
+
+#endif  /* #define __SKETCHING_OPERATIONS_H__ */
---
+++ 3_Implementation/src/sketching_operations.c
@@ -0,0 +1,71 @@
+/**
+ * @file calculator_operations.c
+ * @author Shankar Balu 
+ * @brief  Function definitions for sketching operations
+ * @version 0.1
+ * @date 2021-04-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <sketching_operations.h>
+
+int sketcha()
+{
+    printf("_______________\n");
+    printf(" |          |  \n");
+    printf(" |             \n");
+    printf(" |             \n");
+    printf(" |             \n");
+    printf(" |             \n");
+    printf("_|_____________\n");
+    return 1;
+}
+
+int sketchb()
+{
+    printf("_______________\n");
+    printf(" |         |   \n");
+    printf(" |         0   \n");
+    printf(" |             \n");
+    printf(" |             \n");
+    printf(" |             \n");
+    printf("_|_____________\n");
+    return 2;
+}
+
+int sketchc()
+{
+    printf("_______________\n");
+    printf(" |         |   \n");
+    printf(" |         0   \n");
+    printf(" |         |   \n");
+    printf(" |         |   \n");
+    printf(" |             \n");
+    printf("_|_____________\n");
+    return 3;
+}
+
+int sketchd()
+{
+    printf("_______________\n");
+    printf(" |         |   \n");
+    printf(" |         0   \n");
+    printf(" |        /|\\ \n");
+    printf(" |         |   \n");
+    printf(" |             \n");
+    printf("_|_____________\n");
+    return 4;
+}
+
+int sketche()
+{
+    printf("_______________\n");
+    printf(" |         |   \n");
+    printf(" |         0   \n");
+    printf(" |        /|\\ \n");
+    printf(" |         |   \n");
+    printf(" |        / \\ \n");
+    printf("_|_____________\n");
+    return 5;
+}
